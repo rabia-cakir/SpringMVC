6f603b516846124584a6a51f2f9fb71d7d0e6e43
kodluyoruz <rabiacakir0681@gmail.com>
2022-01-28 16:24:16 +0300

    RequestParam annotation

--- DemoMVC/src/main/java/com/telusko/AddController.java
+++ DemoMVC/src/main/java/com/telusko/AddController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.telusko.service.AddService;
@@ -14,11 +15,11 @@ public class AddController {
 	
 	
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response)
+	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j,HttpServletRequest request, HttpServletResponse response)
 	{
 		
-		int i=Integer.parseInt(request.getParameter("t1"));
-		int j=Integer.parseInt(request.getParameter("t2"));
+		//int i=Integer.parseInt(request.getParameter("t1"));
+		//int j=Integer.parseInt(request.getParameter("t2"));
 		
 		AddService as=new AddService();
 		int k=as.add(i, j);
