7450d2cd7807c070e603fad79dd237f3ae590f97
kodluyoruz <rabiacakir0681@gmail.com>
2022-01-28 16:17:23 +0300

    Annotation Controller, Configuration, RequestMapping, ComponentScan

--- DemoMVC/src/main/java/com/telusko/AddController.java
+++ DemoMVC/src/main/java/com/telusko/AddController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.telusko.service.AddService;
+
 @Controller
 public class AddController {
 	
@@ -18,11 +20,12 @@ public class AddController {
 		int i=Integer.parseInt(request.getParameter("t1"));
 		int j=Integer.parseInt(request.getParameter("t2"));
 		
-		int k=i+j;
+		AddService as=new AddService();
+		int k=as.add(i, j);
 		
 		//we can pass the data which we have generated
 		ModelAndView mv=new ModelAndView();
-		mv.setViewName("display.jsp");
+		mv.setViewName("display");
 		mv.addObject("result",k);
 		
 		
---
+++ DemoMVC/src/main/java/com/telusko/service/AddService.java
@@ -0,0 +1,10 @@
+package com.telusko.service;
+
+public class AddService {
+	
+	public int add(int i, int j)
+	{
+		return i+j;
+	}
+
+}
